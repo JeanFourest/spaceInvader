fe8b166dbfeb36be0cdd0979dd4a8ad80bbdefec
fabien <fabien.arnal@edu.esiee-it.fr>
2023-02-06 16:12:23 +0100

    partie front

--- main.js
+++ main.js
@@ -1,11 +1,3 @@
-const startGameButton = document.querySelector("#start-game-button");
-
-startGameButton.addEventListener("click", function() {
-    // Code to start the game goes here
-    console.log("The game has started!");
-});
-
-
 
 
 const jeu = document.querySelector(".jeu");
