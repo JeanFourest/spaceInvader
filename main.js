3d060eacde138b7e1ac4fa10a418fc40fc8def11
fabien <fabien.arnal@edu.esiee-it.fr>
2023-02-06 13:20:11 +0100

    front

---
+++ main.js
@@ -0,0 +1,6 @@
+const startGameButton = document.querySelector("#start-game-button");
+
+  startGameButton.addEventListener("click", function() {
+    // Code to start the game goes here
+    console.log("The game has started!");
+  });
